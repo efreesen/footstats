6b3db5ba58ce1286fefeaa8f85972ae17aef1e0f
Caio Torres <efreesen@gmail.com>
2015-03-25 16:09:49 -0300

    Correção do nome dos campos

--- lib/footstats/api/racing/ranking.rb
+++ lib/footstats/api/racing/ranking.rb
@@ -2,10 +2,15 @@ require 'footstats/resource'
 
 class Footstats::Api::Racing::Ranking < Footstats::Resource
   field :position, :Posicao
+  field :id, :Id
   field :driver_id, :IdPiloto
-  field :driver_name, :NomePiloto
+  field :name, :Nome
+  field :points, :Pontos
   field :best_lap, :MelhorVolta
   field :team, :Escuderia
   field :acronym, :Sigla
   field :type, :Tipo
+  field :victories, :Vitorias
+  field :pit_stops, :PitStop
+  field :team_name, :NomeEscuderia
 end
